ab48fb82a061007ebbf1c6e65d81ea3b9335153d
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2026-07-12 16:22:27 +0700

    Test CI

---
+++ business/user-service/src/main/java/com/blog/userservice/controllers/ProfileController.java
@@ -0,0 +1,21 @@
+/*
+ * ProfileController.java
+ *
+ * Copyright (c) 2025 Nguyen. All rights reserved.
+ * This software is the confidential and proprietary information of Nguyen.
+ */
+
+package com.blog.userservice.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ProfileController.java
+ *
+ * @author Nguyen
+ */
+@RestController
+@RequestMapping("/api/profile")
+public class ProfileController {
+}
